a67d4070da4c7ac360b7671d2c8531b5f2fc913e
matirene <matirene1@hotmail.com>
2022-12-10 20:49:20 -0300

    Primera practica finalizada

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import SideBar from './components/SideBar'
+import ContentWrapper from './components/ContentWrapper';
 import './assets/css/app.css'
 
 function App() {
   return (
     <div id="wrapper">
       <SideBar/>
+      <ContentWrapper/>
     </div>
   );
 }
---
+++ src/components/ContentWrapper.js
@@ -0,0 +1,21 @@
+import TopBar from './TopBar';
+import ContentRowTop from './ContentRowTop';
+import Footer from './Footer';
+
+function ContentWrapper() {
+    return(
+        <div id="content-wrapper" className="d-flex flex-column">
+
+        {/*<!-- Main Content -->*/}
+        <div id="content">
+            <TopBar/>
+            <ContentRowTop/>
+        </div>
+
+        <Footer/>
+
+        </div>
+    )
+}
+
+export default ContentWrapper;
