380ee8bf1ab326a4dcffe0e646384bf4f8dc9aef
matirene <matirene1@hotmail.com>
2022-12-12 21:25:28 -0300

    Segunda ejercitacion

---
+++ src/components/Card.js
@@ -0,0 +1,30 @@
+function Card(props) {
+  return (
+    <div className="col-md-4 mb-4">
+      <div className={`card border-left-${props.color} shadow h-100 py-2`}>
+        <div className="card-body">
+          <div className="row no-gutters align-items-center">
+            <div className="col mr-2">
+              <div className={`text-xs font-weight-bold text-${props.color} text-uppercase mb-1`}>
+                {props.title}
+              </div>
+              <div className="h5 mb-0 font-weight-bold text-gray-800">{props.number}</div>
+            </div>
+            <div className="col-auto">
+              <i className={`fas fa-${props.icon} fa-2x text-blue-300`}></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+Card.defaultProps={
+    title: "Sin titulo",
+    color: "danger",
+    number: "0",
+    icon: "user-check"
+}
+
+export default Card;
--- src/components/ContentRowMovies.js
+++ src/components/ContentRowMovies.js
@@ -1,71 +1,38 @@
+import Card from "./Card";
+
 function ContentRowMovies() {
-    return(
-      <div className="row">
-      {/*<!-- Movies in Data Base -->*/}
-      <div className="col-md-4 mb-4">
-        <div className="card border-left-primary shadow h-100 py-2">
-          <div className="card-body">
-            <div className="row no-gutters align-items-center">
-              <div className="col mr-2">
-                <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                  Movies in Data Base
-                </div>
-                <div className="h5 mb-0 font-weight-bold text-gray-800">
-                  21
-                </div>
-              </div>
-              <div className="col-auto">
-                <i className="fas fa-film fa-2x text-gray-300"></i>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+  let moviesInDB = {
+    title: "Movies in Data Base",
+    color: "primary",
+    number: "21",
+    icon: "clipboard-list",
+  };
 
-      {/*<!-- Total awards -->*/}
-      <div className="col-md-4 mb-4">
-        <div className="card border-left-success shadow h-100 py-2">
-          <div className="card-body">
-            <div className="row no-gutters align-items-center">
-              <div className="col mr-2">
-                <div className="text-xs font-weight-bold text-success text-uppercase mb-1">
-                  {" "}
-                  Total awards
-                </div>
-                <div className="h5 mb-0 font-weight-bold text-gray-800">
-                  79
-                </div>
-              </div>
-              <div className="col-auto">
-                <i className="fas fa-award fa-2x text-gray-300"></i>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+  let totalAwards = {
+    title: " Total awards",
+    color: "success",
+    number: "79",
+    icon: "award",
+  };
 
-      {/*<!-- Actors quantity -->*/}
-      <div className="col-md-4 mb-4">
-        <div className="card border-left-warning shadow h-100 py-2">
-          <div className="card-body">
-            <div className="row no-gutters align-items-center">
-              <div className="col mr-2">
-                <div className="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                  Actors quantity
-                </div>
-                <div className="h5 mb-0 font-weight-bold text-gray-800">
-                  49
-                </div>
-              </div>
-              <div className="col-auto">
-                <i className="fas fa-user fa-2x text-gray-300"></i>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+  let actorsQuantity = {
+    title: "Actors quantity",
+    color: "warning",
+    number: "49",
+    icon: "user-check",
+  };
+
+  let cardProps = [moviesInDB, totalAwards, actorsQuantity];
+
+  return (
+    <div className="row">
+      {cardProps.map((element, index) => {
+        return <Card
+          {...element} key={index}
+        />
+      })}
     </div>
-    )
+  );
 }
 
 export default ContentRowMovies;
